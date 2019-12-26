ba6f4803d7a26d37c91f69d3b207839143558320
LeHongThang <55943731+LeHongThang@users.noreply.github.com>
2019-12-26 09:26:02 +0700

    add sendemail

---
+++ myproject/app/Mail/SendMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $demo;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($demo)
+    {
+        $this->demo = $demo; 
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mail.sendmail')
+                    ->text('mail.sendmail_plain')
+                    ->with([
+                        'gia_tri_1' => 1
+                        'gia_tri_2' =>100
+                    ])
+                    ->attach(public_path('/images').'demo/jpg', [
+                        #đính kèm (optional)
+                        'as' => 'demo.jpg',
+                        'mine' =>'images/jpeg',
+                    ]);
+    }
+}
